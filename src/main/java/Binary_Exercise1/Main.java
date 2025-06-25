e4013c37a8615a025efb09e9262eae5d16b93e37
ChrisCode3 <malusdarkblade66@gmail.com>
2025-06-25 22:01:08 +0100

    first commit

---
+++ src/main/java/Binary_Exercise1/Main.java
@@ -0,0 +1,13 @@
+package Binary_Exercise1;
+
+/**
+ * Hello world!
+ *
+ */
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/Binary_Exercise2/Main.java
@@ -0,0 +1,9 @@
+package Binary_Exercise2;
+
+public class Main
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "hello darknesssadasd" );
+    }
+}
