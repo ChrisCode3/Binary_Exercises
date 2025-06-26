12650aac84e83259e5d3b8f812e4b64e2b169d1d
ChrisCode3 <malusdarkblade66@gmail.com>
2025-06-26 20:40:46 +0100

    second commit

--- src/main/java/Binary_Exercise1/Main.java
+++ src/main/java/Binary_Exercise1/Main.java
@@ -6,8 +6,12 @@ package Binary_Exercise1;
  */
 public class Main
 {
-    public static void main( String[] args )
-    {
+    public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+
+
+
+
+
     }
 }
--- src/main/java/Binary_Exercise2/Main.java
+++ src/main/java/Binary_Exercise2/Main.java
@@ -1,9 +1,19 @@
 package Binary_Exercise2;
 
+import java.util.Locale;
+
 public class Main
 {
     public static void main( String[] args )
     {
         System.out.println( "hello darknesssadasd" );
+
+        String grteeting = "hello";
+
+
+
+
+        System.out.println( grteeting );
+
     }
 }
