97f07138f7f99e6b44bb6bd9739fb42de423c422
ChrisCode3 <malusdarkblade66@gmail.com>
2025-08-05 19:22:47 +0100

    added scanner class - refactored the comments

--- src/main/java/Binary_Exercise2/Binary_Calculator_Multiplication.java
+++ src/main/java/Binary_Exercise2/Binary_Calculator_Multiplication.java
@@ -2,12 +2,12 @@ package Binary_Exercise2;
 
 import java.util.Arrays;
 
-
+import java.util.Scanner;
 
 public class Binary_Calculator_Multiplication {
 
 
-    //// This is the section where we separate the numb1 into single digits into an arr
+ /*   //// This is the section where we separate the numb1 into single digits into an arr
     public static int [] digitSeparate1 ( int arr [] , int numb1 ) {
 
         int i = 0;
@@ -23,9 +23,9 @@ public class Binary_Calculator_Multiplication {
 
         }
         return arr;
-    }
+    }*/
 
-
+/*
     //// This is the section where we separate the numb2 into single digits into an arr2
     public static int [] digitSeparate2 ( int arr2 [] , int numb2 ) {
 
@@ -41,143 +41,11 @@ public class Binary_Calculator_Multiplication {
             i = i +1;
         }
         return arr2;
-    }
-
-
-
-/*
-// This is the method where we create size of the multiArr
-    public static int [][] createMultiArr ( int columnSize , int rowSize ) {
-
-        int multiArr[][] = new int[rowSize][columnSize + 1];
-
-        return  multiArr;
-    }
-*/
-
-
-
-    // This is the section where we do the mutliplication of the arr and arr2 and we place it into the multiArr
-
-  /*  public static int [][] binaryMultiplication ( int arr [] , int arr2 []  , int multiArr [][] , int columnSize , int rowSize ) {
-
-
-        // These 2 variables are used to control the added zeros we need to put in the end of the array.
-        boolean isTrue = false;
-        int isTrueCounter = 0;
-
-        int numberforZeros = columnSize ;
-
-        for (int y = rowSize - 1; y >= 0; y--) {
-
-            if (isTrue) {
-                isTrueCounter = isTrueCounter + 1;
-
-            }
-
-
-            for (int x = columnSize - rowSize, z = numberforZeros - isTrueCounter ; x >= 0 && z >= 0; x--, z--) {
-
-                isTrue = true;
-
-                multiArr[y][z] = arr[x] * arr2[y];
-                System.out.print(" ([" + z + "][" + y + "]=0 hello + " + multiArr[y][z] + "/");
-
-                //  multiArr[y][z] = arr[x] * arr2[y] ;
-
-
-                System.out.println(Arrays.deepToString(multiArr));
-            }
-            System.out.println(" ");
-            System.out.println("Array contents: y = " + y + "/" + Arrays.deepToString(multiArr));
-
-            System.out.println(" ");
-
-        }
-
-
-        return multiArr;
-    }
-
-
-    // in this method here we just print the array, so we can visually understand on how the addition will happen.
-    public static void printMultiArr (int multiArr [][] ) {
-
-
-        for (int l = multiArr.length - 1; l >= 0; l--) {
-
-            System.out.println(Arrays.toString(multiArr[l]));
-
-
-        }
-
-        System.out.println("  "  );
-
-    }
-
-
-
-    // In this section we will perform the addition with the binary number  - calculating the carry over and the result
-    public static int binaryAddition (int[][] multiArr, int columnSize , int rowSize  ) {
-        int result;
-        int carryOver = 0;
-        int endResult = 0;
-        boolean carrOverCondition = false;
-        int endResultSingleNumb = 0;
-        int multiplier = 0;
-
-
-        for (int i = columnSize ; i >= 0; i--) {
-
-            if (!carrOverCondition) {
-                result = 0;
-            } else {
-                result = carryOver;
-            }
-
-            for (int j = 0; j < rowSize; j++)  {
-
-              result = multiArr[j][i]  + result ;
-
-                carryOver =   result / 2;
-
-                carrOverCondition = true;
-
-                endResult = result % 2;
-
-                System.out.print(" ([" + i + "][" + j + "] numbAr" + multiArr[j][i] + "/ result =" + result + " carryover = " + carryOver  + " endResult " + endResult);
-
-            }
-
-            endResultSingleNumb =  addZeros(endResult , endResultSingleNumb, multiplier );
-
-            multiplier = multiplier +1;
-
-            System.out.println("  "  );
-
-
-        }
-      return   endResultSingleNumb;
-    }
-
-
-    // this is the method where we take the end result and create 1 single integer to it. essentially each digit is multiplipled
-    public static int addZeros(int endResult, int endResultSingleNumb, int multiplier) {
-
-        endResultSingleNumb= (int) (endResult * Math.pow(10, multiplier) + endResultSingleNumb);
-
-        return endResultSingleNumb;
-
-    }
-*/
-
+    }*/
 
 
     public static void main(String[] args) {
 
-
-
-/*
 
         Scanner myObj = new Scanner(System.in);  // Create a Scanner object
         System.out.println("Enter the first number you want to ");
@@ -186,19 +54,15 @@ public class Binary_Calculator_Multiplication {
 
         System.out.println("Enter the second number");
         int numb2 = myObj.nextInt();  // Read user input
+
+        System.out.println("  "  );
         System.out.println("First binary number " +  numb1 );  // Output user input
 
         System.out.println("Second binary number " +  numb2 );  // Output user input
-*/
 
+        System.out.println("  "  );
+        System.out.println("If you multiply them " );  // Output user input
 
-
-        int numb1 = 100;
-        int numb2 = 111;
-
-        //   int firstNumber = numb1;
-
-        //   int secondNumber = numb2;
 
         int lengthofnumb1 = String.valueOf(numb1).length();
         int lengthofnumb2 = String.valueOf(numb2).length();
@@ -207,60 +71,22 @@ public class Binary_Calculator_Multiplication {
         int arr2[] = new int[lengthofnumb2 ];
 
 
-        //    digitSeparate1(arr , numb1);
-
-        //  digitSeparate2(arr2 ,numb2 );
-
         final int columnSize = lengthofnumb1 + lengthofnumb2  - 1 ;
         final int rowSize = lengthofnumb2 ;
 
-
-
-    /*    int separatedDigits1 [] = digitSeparate1(arr, numb1);
-        int separatedDigits2 [] = digitSeparate2(arr2, numb2);
-        int multiplicationArr [][] = createMultiArr(columnSize, rowSize);
-
-
-       int multiplicationResult [][]  = binaryMultiplication(separatedDigits1, separatedDigits2, multiplicationArr, columnSize, rowSize);
-
-
-        printMultiArr(multiplicationResult);
-
-       int end = binaryAddition(multiplicationResult, columnSize, rowSize);
-
-        System.out.println(" " );
-         System.out.println(" Binary number is  " +  end);
-*/
-
-
-
-
-
-
-        //We create the specific size of the multiArr with the rowSize and columnSize +1
+        // We create the specific size of the multiArr with the rowSize and columnSize +1
         int multiArr[][] = new int[rowSize][columnSize + 1];
-
-
-
-        /*------------------------------------------------------------------------------------------------------------------------*/
-
-
-        //    int i = 0;
-
-        //    int j = 0;
-
 
 
 //// This is the section where we separate the numb1 into single digits into an arr
         for (int i = lengthofnumb1 - 1; i >= 0; i--) {
 
             arr[i] = numb1 % 10; // store last digit
-            System.out.println("arr[ " + i + "] " + arr[i]);
+           // System.out.println("arr[ " + i + "] " + arr[i]);
             numb1 = numb1 / 10; // chop  out last digit number
 
-            //  i = i + 1;
-        }
 
+        }
 
 
 //// This is the section where we separate the numb1 into single digits into an arr
@@ -268,8 +94,8 @@ public class Binary_Calculator_Multiplication {
 
             arr2[l] = numb2 % 10; // store last digit
 
-            System.out.println(" we are here  ");
-            System.out.println("arr2[ " + l + "] " + arr2[l]);
+          //  System.out.println(" we are here  ");
+          //  System.out.println("arr2[ " + l + "] " + arr2[l]);
 
             numb2 = numb2 / 10; // chop  out last digit number
 
@@ -277,8 +103,8 @@ public class Binary_Calculator_Multiplication {
 
         }
 
-        System.out.println(Arrays.toString(arr));
-        System.out.println(Arrays.toString(arr2));
+    //    System.out.println(Arrays.toString(arr));
+     //   System.out.println(Arrays.toString(arr2));
 
 
 
@@ -290,10 +116,10 @@ public class Binary_Calculator_Multiplication {
 
         int numberforZeros = columnSize ;
 
-        System.out.println(" rowSize    =  " + rowSize);
+      //  System.out.println(" rowSize    =  " + rowSize);
 
-        System.out.println(" columnSize =  " + columnSize);
-
+      //  System.out.println(" columnSize =  " + columnSize);
+        System.out.println("  "  );
 
         for (int y = rowSize - 1; y >= 0; y--) {
 
@@ -308,17 +134,17 @@ public class Binary_Calculator_Multiplication {
                 isTrue = true;
 
                 multiArr[y][z] = arr[x] * arr2[y];
-                System.out.print(" ([" + z + "][" + y + "]=0 hello + " + multiArr[y][z] + "/");
+             //   System.out.print(" ([" + z + "][" + y + "]=0 hello + " + multiArr[y][z] + "/");
 
                 //  multiArr[y][z] = arr[x] * arr2[y] ;
 
 
-                System.out.println(Arrays.deepToString(multiArr));
+          //      System.out.println(Arrays.deepToString(multiArr));
             }
-            System.out.println(" ");
-            System.out.println("Array contents: y = " + y + "/" + Arrays.deepToString(multiArr));
+        //    System.out.println(" ");
+         //   System.out.println("Array contents: y = " + y + "/" + Arrays.deepToString(multiArr));
 
-            System.out.println(" ");
+         //   System.out.println(" ");
 
         }
 
@@ -330,12 +156,6 @@ public class Binary_Calculator_Multiplication {
 
 
         }
-
-        //    System.out.println(Arrays.deepToString(multiArr));
-        System.out.println(" ");
-        System.out.println(" ");
-
-
 
         // This section here is to do the addiction and use the carryover.
 
@@ -373,24 +193,29 @@ public class Binary_Calculator_Multiplication {
 
                 endResult = result % 2;
 
-                System.out.print(" ([" + i + "][" + j + "] numbAr" + multiArr[j][i] + "/ result =" + result + " carryover = " + carryOver  + " endResult " + endResult);
+          //      System.out.print(" ([" + i + "][" + j + "] numbAr" + multiArr[j][i] + "/ result =" + result + " carryover = " + carryOver  + " endResult " + endResult);
 
             }
             endResultSingleNumb  = (int) (endResult * Math.pow(10, multiplier) + endResultSingleNumb );
 
             multiplier = multiplier +1;
 
-            System.out.println("  "  );
+
 
 
         }
+
+        System.out.println("------------------" );
         System.out.println("  "  );
 
-        //  System.out.println("  The binary multiplication of " + firstNumber + " and " +  secondNumber + " is" );
-        System.out.println( "                   " + endResultSingleNumb);
+        System.out.println("  The result is:");
 
 
+        System.out.println("    " +
+                "  "   + endResultSingleNumb);
     }
+
+
 
 }
 
